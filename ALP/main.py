4f2a7df9cd665f908f1c1177372cd17b3bb30a37
Chase Durand <chase.durand@gmail.com>
2020-12-30 22:33:19 -0600

    Batch unzips all als files in a given directory.

--- ALP/main.py
+++ ALP/main.py
@@ -1,7 +1,32 @@
 import xml.etree.ElementTree as ET
+import gzip
+import shutil
+import os
+import glob
+import collections
 
-#tree = ET.parse('/Users/chasedurand/Desktop/SET1.xml') #get XML file
-tree = ET.parse('/Users/chasedurand/Desktop/DNB.xml') #get XML file
+projects = []
+projectsSet = set()
+projectsAll = []
+
+for file in glob.glob('/Users/chasedurand/Desktop/ZIPPED/**/*.als'):
+    projects.append(file)
+
+count = 1
+total = len(projects)
+
+
+for i in projects:
+    destination = os.path.split(os.path.split(i)[0])[1] + ' - ' + os.path.splitext((os.path.split(i)[1]))[0]
+    #destination = destination
+    destination+='.xml'
+    destination = '/Users/chasedurand/Desktop/UNZIPPED/' + destination
+    #with gzip.open(i, 'r') as f_in, open(destination, 'wb') as f_out:
+    #    shutil.copyfileobj(f_in, f_out)
+    print('(',count,'/',total,')',destination)
+    count += 1
+'''
+tree = ET.parse() #get XML file
 root = tree.getroot()
 
 sample_path = ""
@@ -16,3 +41,4 @@ for sample_element in root.iter('SampleRef'): #iterate over all sample reference
                 sample_path += '/' + filename.get('Value')
                 sample_set.add(sample_path)
 print("\n".join(sorted(sample_set)))
+'''
