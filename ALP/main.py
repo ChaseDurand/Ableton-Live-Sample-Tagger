a791fa16415536ce8f544cfbe0098573bc96745c
Chase Durand <chase.durand@gmail.com>
2021-01-16 21:33:32 -0500

    Added very very hacky hex parsing to find full sample filepath. Need to do more testing to see if it catches all cases.

--- ALP/main.py
+++ ALP/main.py
@@ -3,50 +3,85 @@ import gzip
 import shutil
 import os
 import glob
-import collections
 
 projects = []
 
 print('Finding .als files.')
 
-for file in glob.glob('/Users/chasedurand/Desktop/ZIPPED2/**/*.als', recursive=True): #Find all als files in all subdirectories
-    if '/Backup/' not in file: #Filter to ignore backup als files and add remaining to list
+for file in glob.glob('/Users/chasedurand/Desktop/ZIPPED3/**/*.als',
+                      recursive=True):  # Find all als files in all subdirectories
+    if '/Backup/' not in file:  # Filter to ignore backup als files and add remaining to list
         projects.append(file)
 
-count_projects = 1
+count = 1
 total_projects = len(projects)
 
-print(total_projects, '.als files found.')
+print(total_projects, '.als files found.\n')
 
 print('Converting .als to .xml.')
 
 for i in projects:
     destination = os.path.split(os.path.split(i)[0])[1] + ' - ' + os.path.splitext((os.path.split(i)[1]))[0]
-    destination+='.xml'
-    destination = '/Users/chasedurand/Desktop/UNZIPPED2/' + destination
+    destination += '.xml'
+    destination = '/Users/chasedurand/Desktop/UNZIPPED3/' + destination
     with gzip.open(i, 'r') as f_in, open(destination, 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
-    print('(', count_projects, '/', total_projects, ') ', destination, sep='')
-    projects[count_projects-1] = destination
-    count_projects += 1
+    print('(', count, '/', total_projects, ') Converting ', destination, sep='')
+    projects[count - 1] = destination
+    count += 1
 
-print('.als to .xml conversion complete.')
+print('.als to .xml conversion complete.\n')
 print('Building sample table.')
 
+count = 1
+sample_set = set()
 
-for i in projects:
-    tree = ET.parse(i) #get XML file
+for i in projects:  # iterate over all xml files
+    tree = ET.parse(i)
     root = tree.getroot()
-    print(i)
+    print('(', count, '/', total_projects, ') Finding samples in ', i, sep='')
+    count += 1
     sample_path = ""
-    sample_set = set()
 
-    for sample_element in root.iter('SampleRef'): #iterate over all sample references
-            sample_path = ""
-            for path in sample_element.find('FileRef').find('SearchHint').find('PathHint'):
-                    for path_element in path.iter('RelativePathElement'):
-                            sample_path += '/' + path_element.get('Dir')
-            for filename in sample_element.find('FileRef').iter('Name'):
-                    sample_path += '/' + filename.get('Value')
+
+    for sample_element in root.iter('SampleRef'):  # iterate over all sample references
+        sample_path = ""
+        for path in sample_element.find('FileRef').find('SearchHint').find('PathHint'):
+            for path_element in path.iter('RelativePathElement'):
+                sample_path += '/' + path_element.get('Dir')
+        for filename in sample_element.find('FileRef').iter('Name'):
+            sample_path += '/' + filename.get('Value')
+            sample_set.add(sample_path)
+    '''
+
+    for sample_element in root.iter('SampleRef'):  # iterate over all sample references
+        sample_path = ""
+        for path in sample_element.find('FileRef').find('RelativePath'):
+            for path_element in path.iter('RelativePathElement'):
+                sample_path += '/' + path_element.get('Dir')
+        for filename in sample_element.find('FileRef').iter('Name'):
+            sample_path += '/' + filename.get('Value')
+            sample_set.add(sample_path)
+    '''
+    '''
+    for  sample_element in root.iter('SampleRef'):
+        sample_path = ""
+        for path in sample_element.find('SourceContext'):
+            for path_element in path.iter('BrowserContentPath'):
+                #print(path_element)
+                if "userfolder" in path_element.get('Value'):
+                    sample_path = path_element.get('Value')
+                    #need to parse/clean sample path here
                     sample_set.add(sample_path)
-    print("\n".join(sorted(sample_set)))
+                    #print(sample_path)
+    '''
+count = 1
+total_samples = len(sample_set)
+
+print(total_samples, 'unique samples found.')
+
+sample_set = sorted(sample_set)
+
+for i in sample_set:
+    print('(',count,'/',total_samples,') ',i, sep='')
+    count += 1
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AbletonLiveParser
 
 Ableton Live Parser (ALP) is a utility for gathering and displaying basic data from a collection of Ableton Live Sets (aka project files).
+
+Ideally I would make it do more (for example show fun facts like most used plugins and samples), but the current goal is to find and tag all samples used throughout my projects so I can know which files are safe to delete without breaking any project.
+
+I can not overstate how hacky my implementation is.
