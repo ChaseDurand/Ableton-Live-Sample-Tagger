32500ce8faa9a2ed98c9be8407a117de98c019d8
ChaseDurand <chase.durand@gmail.com>
2021-09-09 17:59:58 -0500

    Added optional input parameters for mostFrequentSamplePairs

--- Analysis/mostFrequentSamplePairs.py
+++ Analysis/mostFrequentSamplePairs.py
@@ -3,14 +3,24 @@ from sqlite3 import Error
 from pathlib import Path
 from itertools import combinations
 import operator
+import sys
 
 print("***WARNING: This operation may take time for large databases***")
-while True:
-    dbPath = Path(input("Enter Ableton Live Projects directory: "))
-    if dbPath.exists():
-        break
-    else:
-        print(dbPath, " is not a valid path!")
+
+if len(sys.argv) == 1:
+    while True:
+        dbPath = Path(input("Enter Ableton Live Projects directory: "))
+        if dbPath.exists():
+            break
+        else:
+            print(dbPath, " is not a valid path!")
+else:
+    dbPath = sys.argv[1]
+
+if len(sys.argv) == 3:
+    pairsToShow = int(sys.argv[2])
+else:
+    pairsToShow = 25
 
 try:
     conn = sqlite3.connect(str(dbPath))
@@ -48,10 +58,8 @@ sortedPairsCount = sorted(pairsCount.items(),
                           reverse=True)
 
 i = 0
-pairsToShow = 10
 output = [[0] * 3 for j in range(pairsToShow)]
 while i < pairsToShow and i < len(sortedPairsCount):
-
     sampleIDa = sortedPairsCount[i:i + 1][0][0][0]
     sampleIDb = sortedPairsCount[i:i + 1][0][0][1]
     output[i][2] = sortedPairsCount[i:i + 1][0][1]
@@ -62,7 +70,12 @@ while i < pairsToShow and i < len(sortedPairsCount):
         """SELECT sampleName FROM samples WHERE sampleID=?;""",
         (sampleIDb, )).fetchone()[0]
     i += 1
+header = ["Sample 1", "Sample 2", "Frequency"]
+output.insert(0, header)
+print()
 for row in output:
-    print("{: >40} {: >40} {: >40}".format(*row))
+    print("{: <40} {: <40} {: <40}".format(*row))
+if i == len(sortedPairsCount):
+    print("No samples remaining.")
 conn.close
 exit()
