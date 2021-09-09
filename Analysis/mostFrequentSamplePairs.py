33b29d6a0b342dc0556ad8919ca7af64a52671c4
ChaseDurand <chase.durand@gmail.com>
2021-09-09 18:13:01 -0500

    Added comments.

--- Analysis/mostFrequentSamplePairs.py
+++ Analysis/mostFrequentSamplePairs.py
@@ -4,9 +4,13 @@ from pathlib import Path
 from itertools import combinations
 import operator
 import sys
+'''
+Get most common pairs of samples from database.
+'''
 
 print("***WARNING: This operation may take time for large databases***")
 
+#If database wasn't given as argument, then prompt user for db path.
 if len(sys.argv) == 1:
     while True:
         dbPath = Path(input("Enter Ableton Live Projects directory: "))
@@ -17,6 +21,7 @@ if len(sys.argv) == 1:
 else:
     dbPath = sys.argv[1]
 
+#If optional int items to show was given as argumnt, then set items to display or default.
 if len(sys.argv) == 3:
     pairsToShow = int(sys.argv[2])
 else:
@@ -27,12 +32,12 @@ try:
 except Error as e:
     print(e)
     exit()
-
 cursor = conn.cursor()
 
 projectSelection = cursor.execute(
     """SELECT DISTINCT projectID FROM projectSampleMapping""")
 pairsCount = {}
+#For each als, get all samples and record combinations
 for als in projectSelection.fetchall():
     setName = cursor.execute(
         """SELECT setName FROM projects WHERE projects.projectID =?;""",
@@ -45,20 +50,22 @@ for als in projectSelection.fetchall():
         (als[0], ))
     for items in sampleSelection.fetchall():
         samples.append(items[0])
-    samples.sort()
+    samples.sort(
+    )  #Sort to ensure pairs are counted in same order (i.e. won't record 1-2 and 2-1 separately)
     combos = combinations(samples, 2)
     for pair in combos:
         if pair in pairsCount:
             pairsCount[pair] += 1
         else:
             pairsCount[pair] = 1
-
+#Sort counts of all combinations
 sortedPairsCount = sorted(pairsCount.items(),
                           key=operator.itemgetter(1),
                           reverse=True)
 
 i = 0
 output = [[0] * 3 for j in range(pairsToShow)]
+#For most common samples, convert sampleID to sampleName for display
 while i < pairsToShow and i < len(sortedPairsCount):
     sampleIDa = sortedPairsCount[i:i + 1][0][0][0]
     sampleIDb = sortedPairsCount[i:i + 1][0][0][1]
